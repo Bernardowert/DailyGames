f2e06a87344f6f50a3d17641d0c53e05f0d0d1d0
devBernardo <bernardowerty@outlook.com>
2025-08-14 00:01:29 -0300

    formulação do componente gameCard e aplicando clean code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { ContainerGRID } from "@/components/containerGRID";
-import { GameCard } from "@/components/gameCard";
-import { Input } from "@/components/input";
-import { RandomGame } from "@/components/randomGame";
 import { SectionGames } from "@/components/sectionComponents/home/sectionGames";
 import { SectionHero } from "@/components/sectionComponents/home/sectionHero";
-import { getDailyGame, getGamesData } from "@/utils/api";
-import { GameProps } from "@/utils/types/game";
 
 
 
 export default async function Home() {
-  const data: GameProps[] = await getGamesData();
   return (
       <>
        <SectionHero/>
--- src/components/gameCard/index.tsx
+++ src/components/gameCard/index.tsx
@@ -11,7 +11,26 @@ interface GameCardProps{
 
 export function GameCard({data}: GameCardProps){
     return(
-        <Link href={`/game/${data.id}`}>
+        <Link href={`/game/${data.id}`} className="w-full h-72 bg-slate-200 rounded-lg p-3 group">
+                <div className="overflow-hidden w-full rounded-lg h-56">
+                    <Image
+                    src={data.image_url}
+                    alt={data.title}
+                    title={data.title}
+                    fill
+                    className="!static object-cover  transition-transform ease-linear group-hover:scale-105"
+                    />
+                </div>
+             <div className="flex items-center justify-between mt-4">
+                <p className="text-sm font-bold text-black text-ellipsis truncate">{data.title}</p>
+                <BiRightArrowCircle size={24} color="#000"/>
+            </div>
+        
+        </Link>
+    )
+}
+
+{/* <Link href={`/game/${data.id}`}>
           <section className="w-full bg-slate-200 rounded-lg p-4 mb-5">
             <div className="relative w-full h-56 hover:scale-105 transition-all duration-300">
                 <Image
@@ -29,6 +48,4 @@ export function GameCard({data}: GameCardProps){
             </div>
          </section>
         
-        </Link>
-    )
-}
+        </Link> */}
