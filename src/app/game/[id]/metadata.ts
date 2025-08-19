35d4470cc8d29924b65c91cc8567d113de11b6af
devBernardo <bernardowerty@outlook.com>
2025-08-19 15:58:05 -0300

    correção de metaData

--- src/app/game/[id]/metadata.ts
+++ src/app/game/[id]/metadata.ts
@@ -1,15 +1,20 @@
 import { getData } from "@/utils/api";
 import { GameProps } from "@/utils/types/game";
 import { Metadata } from "next";
-import { Params } from "next/dist/server/request/params";
 
-export async function generateMetadata({params}:{params:Params}):Promise<Metadata>{
-    const {id} = params;
+
+type Props = {
+   params: Promise<{id:string}>;
+}
+
+export async function generateMetadata({params}:Props):Promise<Metadata>{
+    const {id} = await params;
     const data:GameProps = await getData(String(id));  
     
     if(!data){
           return{
-             title:"Jogo não encontrado"
+             title:"Jogo não encontrado",
+             description: "Descrição não encontrada"
           }
     }
 
